91d1672e4586625821d289ef9cd4f1693a243f32
Huw Williams <huwmorgan91@gmail.com>
2023-04-05 17:20:46 +0100

    One card now displays beer data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.module.scss';
 import BeerCard from './components/BeerCard';
 import NavBar from "./components/NavBar";
-import styles from "./App.module.scss"
+import styles from "./App.module.scss";
+
+import beers from './data/beers';
 
 const App = () => {
   return (
     <>
     <main className={styles.App}>
       <NavBar />
-      <BeerCard />
+      <BeerCard beer={beers[0]}/>
     </main>
     </>
   );
--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from "./BeerCard.module.scss";
 
-function BeerCard() {
+const BeerCard = (props) => {
+
+const {
+  image_url,
+  name,
+  description,
+} = props.beer
+
   return (
     <div className={styles.BeerCard}>
-      <img src="" alt="" />
-      <h1>Name</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam quae molestias laborum voluptatibus architecto et? Doloribus architecto consequuntur cupiditate nemo exercitationem quas. Perferendis inventore expedita totam iure earum labore blanditiis?</p>
+      <img src={image_url} alt="" />
+      <h1>{name}</h1>
+      <p>{description}</p>
     </div>
   )
 }
