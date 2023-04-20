c006ac6095214503c8d6323fab679bbbdd667e31
Huw Williams <huwmorgan91@gmail.com>
2023-04-20 14:25:04 +0100

    Using online API. Classic Range not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BeerCard from './components/BeerCard';
 import NavBar from "./components/NavBar";
 import styles from "./App.module.scss";
-import data from "../src/data/beers"
+
 
 const App = () => {
+
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const getBeers = async () => {
+      try {
+        const response = await fetch("https://api.punkapi.com/v2/beers");
+        const jsonResponse = await response.json();
+        setBeers(jsonResponse);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getBeers();
+  }, []);
 
   const getBeerCard = (beer) => (
     <BeerCard key={beer.id} beer={beer} />
@@ -15,7 +30,7 @@ const App = () => {
   const [beerAge, setBeerAge] = useState(false);
   const [acidity, setAcidity] = useState(false); 
 
-  const filteredData = data.filter((beer) => {
+  const filteredData = beers.filter((beer) => {
     const nameMatches = beer.name.toLowerCase().includes(searchInput.toLowerCase());
     const abvMatches = !highAbv || beer.abv >= 6.0;
     const ageMatches = !beerAge || beer.first_brewed < 2011;
@@ -28,7 +43,7 @@ const App = () => {
       <main className={styles.App}>
         <NavBar setSearchInput={setSearchInput} setHighAbv={setHighAbv} highAbv={highAbv} beerAge={beerAge} setBeerAge={setBeerAge} acidity={acidity} setAcidity={setAcidity} />
         <section className={styles.content}>
-          {filteredData.map(getBeerCard)}
+        {filteredData.map(getBeerCard)}
         </section>
       </main>
     </>
@@ -36,4 +51,5 @@ const App = () => {
 }
 
 export default App;
+
 
